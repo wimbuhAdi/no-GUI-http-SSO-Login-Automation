bfb52bea72a2bfddd41a3a991c282f7e6cc2886e
wimbuhAdi <55055182+wimbuhAdi@users.noreply.github.com>
2020-02-10 04:58:25 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio, time
+from pyppeteer import launch
+UNAME = "UsernameGoesHere"
+PASSWORD = "literalyYourpassword"
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto('https://internet.ugm.ac.id/')
+    await page.click('body > div.px-content > div.col-lg-6.col-md-12.col-sm-12.col-xs-12.button-login > div > div:nth-child(1) > div > a.btn.btn-ugm.btn-lg.btn-block')
+    await page.type('#username',UNAME)
+    await page.type('#password',PASSWORD)
+    await page.click('#fm1 > input.btn.btn-primary')
+    print("Login SSO sucess as wimbuh.tri.hastomo")
+    await asyncio.sleep(2)
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
